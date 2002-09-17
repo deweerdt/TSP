f1f678e28f87fb0c5791ccc504b4679ec0afffc3
fancelli <fancelli>
2002-09-17 09:45:54 +0000

    Evol : corrected ugly bug

--- src/core/misc_utils/tsp_time.c
+++ src/core/misc_utils/tsp_time.c
@@ -1,18 +1,25 @@
 #include "tsp_sys_headers.h"
 
 #include <time.h>
+#include <sys/time.h>
 
 #include "tsp_time.h"
 
 tsp_hrtime_t tsp_gethrtime(void)
 {
+  SFUNC_NAME(tsp_gethrtime);
+
 #ifdef TSP_SYSTEM_HAVE_GETHRTIME
   return gethrtime();
 #else
-  struct timespec tp;
+  struct timeval tp;
   gettimeofday(&tp, (void*)NULL);
-  return ((tsp_hrtime_t)tp.tv_sec *  G_GINT64_CONSTANT(1000000000) + (tsp_hrtime_t)tp.tv_nsec);
+   return ((tsp_hrtime_t)tp.tv_sec *  G_GINT64_CONSTANT(1000000000)
+	   + (tsp_hrtime_t)tp.tv_usec*G_GINT64_CONSTANT(1000));
 #endif
+
+
+  
 
 }
 
