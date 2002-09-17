417b96af6ccdd0492809f0c9169c474864aabb1e
fancelli <fancelli>
2002-09-17 09:45:08 +0000

    Evol : changed posix revision because of dec threads

--- src/core/include/tsp_sys_headers.h
+++ src/core/include/tsp_sys_headers.h
@@ -1,6 +1,6 @@
 /*!  \file 
 
-$Header: /home/def/zae/tsp/tsp/src/core/include/tsp_sys_headers.h,v 1.2 2002-09-05 09:17:17 tntdev Exp $
+$Header: /home/def/zae/tsp/tsp/src/core/include/tsp_sys_headers.h,v 1.3 2002-09-17 09:45:08 fancelli Exp $
 
 -----------------------------------------------------------------------
 
@@ -18,7 +18,7 @@ Purpose   :
 #define __TSP_SYS_HEADERS_H 1
 
 #define _POSIX_SOURCE
-#define _POSIX_C_SOURCE 199309
+#define _POSIX_C_SOURCE 199506L
 
 
 #include <unistd.h>
@@ -27,6 +27,7 @@ Purpose   :
 #include <stdlib.h>
 #include <string.h>
 #include <errno.h>
+
 
 
 #endif
