ca423df50a3dd7234dd4e1f61f08ccdfd640b4d0
galles <galles>
2002-10-01 15:36:20 +0000

    Evol - added define for eof in ringbuf - removed a bunch of unused functions (now the consumer waits for enough room in its ringbuf to ask the provider for new data, and it need the max length group size for this)

--- src/core/driver/tsp_sample_ringbuf.h
+++ src/core/driver/tsp_sample_ringbuf.h
@@ -1,6 +1,6 @@
 /*!  \file 
 
-$Header: /home/def/zae/tsp/tsp/src/core/driver/tsp_sample_ringbuf.h,v 1.1 2002-08-27 08:56:09 galles Exp $
+$Header: /home/def/zae/tsp/tsp/src/core/driver/tsp_sample_ringbuf.h,v 1.2 2002-10-01 15:36:20 galles Exp $
 
 -----------------------------------------------------------------------
 
@@ -21,14 +21,19 @@ the consumer to retreive the n asked symbols
 
 #include "tsp_prjcfg.h"
 
-#include "tsp_datastruct.h"
 #include "tsp_ringbuf.h"
+
+
+/* Used to add a dummy item in fifo for an eof notification */
+#define TSP_DUMMY_PROVIDER_GLOBAL_INDEX_EOF   0xFFFFFFFF
 
 /* Struct to get each sample */
 struct TSP_sample_t
 {
   time_stamp_t time;
+  int provider_global_index;
   double user_value;
+
 
 };
 
@@ -36,10 +41,6 @@ typedef struct TSP_sample_t TSP_sample_t;
 
 /* samples ring buf */
 RINGBUF_DECLARE_TYPE_DYNAMIC(TSP_sample_ringbuf_t,TSP_sample_t);
-
-void TSP_sample_ringbuf_create(
-			       const TSP_sample_symbol_info_list_t* symbols, 
-			       TSP_sample_ringbuf_t** sample_ringbuf);
 
                                        
 #endif /* _TSP_SAMPLE_RINGBUF_H */
