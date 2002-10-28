21d07c518f278a07305c2c09760432cae5743322
galles <galles>
2002-10-28 14:48:51 +0000

    Creation

---
+++ src/core/ctrl_init/tsp_provider_init.c
@@ -0,0 +1,43 @@
+#include "tsp_sys_headers.h"
+
+#include "tsp_provider.h"
+#include "tsp_server.h"
+
+int TSP_provider_init(int* argc, char** argv[])
+{
+  SFUNC_NAME(TSP_provider_init);
+
+  int ret;
+  STRACE_IO(("-->IN"));
+
+  ret = TSP_provider_private_init(argc, argv);
+
+  STRACE_IO(("-->OUT"));
+  return ret;
+
+}
+
+int TSP_provider_run(int blocking)
+{
+  SFUNC_NAME(TSP_provider_run);
+
+  int ret = FALSE;
+  STRACE_IO(("-->IN"));
+
+
+  if(TSP_provider_is_initialized())
+    {            
+      int server_number = TSP_provider_get_server_number();
+      ret = TSP_command_init(server_number, blocking);
+    }
+  else
+    {
+      STRACE_ERROR(("Call TSP_provider_init first, and then call TSP_provider_run ! "))
+    }
+  
+  STRACE_IO(("-->OUT"));
+  return ret;  
+  
+}
+
+
---
+++ src/core/ctrl_init/tsp_provider_init.h
@@ -0,0 +1,11 @@
+#ifndef _TSP_PROVIDER_INIT_H
+#define _TSP_PROVIDER_INIT_H
+
+
+int TSP_provider_init(int* argc, char** argv[]);
+
+int TSP_provider_run(int blocking);
+
+
+
+#endif /* _TSP_PROVIDER_INIT_H */
