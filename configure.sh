6e71c792a2df46239b5a94dcf0e2d9fe5df5f877
tsp_admin <tsp_admin>
2003-02-11 15:20:10 +0000

    Hide login template files

--- configure.sh
+++ configure.sh
@@ -8,12 +8,13 @@ echo "(FIXME 1 : This is NOT a real Autoconf/Automake configure )"
 echo "----------------------------------------------------------"
 
 PWDPATH=`pwd`
-GENSCRIPT="${PWDPATH}/src/scripts/tsp_gen_dev.login.sh"
-TEMPLATE="${GENSCRIPT}.templ"
+WHERE="${PWDPATH}/src/scripts"
+GENSCRIPT="${WHERE}/tsp_dev.login.sh"
+TEMPLATE="${WHERE}/.templ_tsp_dev.login.sh"
 SEDEXP="s:%%tsp_base%%:${PWDPATH}:"
 
 sed -e "$SEDEXP" < $TEMPLATE > $GENSCRIPT
-
+chmod a+x $GENSCRIPT
 echo "${GENSCRIPT} was created"
 echo ""
 echo "Now you should type the following commands : "
@@ -27,3 +28,8 @@ echo ""
 echo "The latest command launch some autotest to check that"
 echo "everything's all right."
 echo ""
+
+
+
+
+
