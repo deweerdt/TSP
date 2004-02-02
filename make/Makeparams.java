870232b6b495830d1be4d6caa3228ecb627d2bda
dufy <dufy>
2004-02-02 12:09:58 +0000

    Nop

--- make/Makeparams.java
+++ make/Makeparams.java
@@ -14,14 +14,17 @@ include $(DEVBASE)/make/Makeproject
 # Using standard tools
 JAVA = java
 JAVAC = javac
-# Use RemoteTea Java RPCGEN (see: http://acplt.plt.rwth-aachen.de/ks/english/remotetea.html)
+JAR = jar
+
+JARDIR= $(DEVBASE)/external
+
+# RemoteTea Java RPCGEN (http://acplt.plt.rwth-aachen.de/ks/english/remotetea.html)
 # Customize your installation of RemoteTea
-JRPCBASE = $(DEVBASE)/external/RemoteTea
+JRPCBASE = $(JARDIR)/RemoteTea
 JRPCGENJAR = $(JRPCBASE)/jrpcgen.jar
 JRPCJAR = $(JRPCBASE)/oncrpc.jar
 JRPCPORTMAPJAR = $(JRPCBASE)/jportmap.jar
 JRPCGEN = $(JAVA) -jar $(JRPCGENJAR)
-JAR = jar
 
 # Default options
 # ---------------
