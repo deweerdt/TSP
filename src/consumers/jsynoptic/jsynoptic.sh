2a87e55529db947ec3bcebde9d29d9391837e989
dufy <dufy>
2004-02-20 17:32:12 +0000

    Simplify script : Thank you Nicolas

--- src/consumers/jsynoptic/jsynoptic.sh
+++ src/consumers/jsynoptic/jsynoptic.sh
@@ -4,9 +4,9 @@
 # Allow to plot symbols in java GUI
 #========================================
 
-export JAR_DIR=${DEVBASE}/external
-export JSYN_DIR=${JAR_DIR}/jSynoptic
-export EXEC_DIR=${DEVBASE}/exec/DEV/java/classes
+JAR_DIR=${DEVBASE}/external
+JSYN_DIR=${JAR_DIR}/jSynoptic
+EXEC_DIR=${DEVBASE}/exec/DEV/java/classes
 
-java -classpath ${EXEC_DIR}:${JSYN_DIR}/jcommon.jar:${JSYN_DIR}/jfreechart.jar:${JSYN_DIR}/jsynoptic.jar:${JAR_DIR}/RemoteTea/oncrpc.jar:${JSYN_DIR}/simtools.jar jsynoptic.ui.Run
+java -classpath ${EXEC_DIR}:${JAR_DIR}/RemoteTea/oncrpc.jar -jar ${JSYN_DIR}/jsynoptic.jar
 
