eea09619d6e14ff849afa05577fb99c55897cd95
dufy <dufy>
2004-03-26 18:27:13 +0000

    Use the tsp.tar

--- src/consumers/jsynoptic/jsynoptic.sh
+++ src/consumers/jsynoptic/jsynoptic.sh
@@ -6,7 +6,7 @@
 
 JAR_DIR=${DEVBASE}/external
 JSYN_DIR=${JAR_DIR}/jSynoptic
-EXEC_DIR=${DEVBASE}/exec/DEV/java/classes
+EXEC_DIR=${DEVBASE}/exec/DEV/java
 
-java -classpath ${EXEC_DIR}:${JAR_DIR}/RemoteTea/oncrpc.jar -jar ${JSYN_DIR}/jsynoptic.jar
+java -classpath ${EXEC_DIR}/jsynoptic-tsp-plugin.jar:${JAR_DIR}/RemoteTea/oncrpc.jar -jar ${JSYN_DIR}/jsynoptic.jar
 
