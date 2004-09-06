48ad9810f84ac00b64f85a1b1cce3f4d414ff100
dufy <dufy>
2004-09-06 11:51:31 +0000

    SFUNC_NAME is now useless and should be removed everywhere

--- src/core/include/tsp_simple_trace.h
+++ src/core/include/tsp_simple_trace.h
@@ -1,6 +1,6 @@
 /*!  \file 
 
-$Header: /home/def/zae/tsp/tsp/src/core/include/Attic/tsp_simple_trace.h,v 1.8 2004-08-31 09:58:52 dufy Exp $
+$Header: /home/def/zae/tsp/tsp/src/core/include/Attic/tsp_simple_trace.h,v 1.9 2004-09-06 11:51:31 dufy Exp $
 
 -----------------------------------------------------------------------
 
@@ -39,39 +39,42 @@ Purpose   : Some trace macro
 #define __TSP_SIMPLE_TRACE_H
 
 #include <stdio.h>
-static int _strace_debug_level = -1;
 
-#define SIMPLE_TRACE(level,src,text) \
+#define SIMPLE_TRACE_FUNC(level,src,func,text) \
 { \
+    static int _strace_debug_level = -1; \
     if (_strace_debug_level<0) \
         if (getenv ("STRACE_DEBUG") != (char *)0) \
             _strace_debug_level = atoi(getenv("STRACE_DEBUG")); \
         else \
             _strace_debug_level = 0; \
-    if (_strace_debug_level >= level ) \
-        { \
-	        printf("%7s||%s##%s##%d: ", src, __FILE__, strace_szfunc, __LINE__); \
-            printf text; printf("\n"); fflush(stdout);\
+    if (_strace_debug_level >= level ) { \
+           printf("%7s||%s##%s##%d: ", src, __FILE__, func, __LINE__); \
+           printf text; printf("\n"); fflush(stdout);\
         } \
 } 
+#ifdef __GNUC__       
+#  define SIMPLE_TRACE(level,src,text) SIMPLE_TRACE_FUNC(level,src,__FUNCTION__,text)
+#else 
+#  define SIMPLE_TRACE(level,src,text) SIMPLE_TRACE_FUNC(level,src,"where?",text)
+#endif
 				  
-				 
+/* deprecated */				 
+#define SFUNC_NAME(name)
 
 #ifdef  USE_STRACE
-# define SFUNC_NAME(name)  static const char* strace_szfunc = #name 
 # define STRACE_IO(text) SIMPLE_TRACE(5, "i/o", text)
 # define STRACE_DEBUG(text) SIMPLE_TRACE(4, "debug", text)
 # define STRACE_INFO(text) SIMPLE_TRACE(3, "Info", text)
 # define STRACE_TEST(text) SIMPLE_TRACE(1, "TEST", text)
 #else
-# define SFUNC_NAME(name)  static const char* strace_szfunc = #name
 # define STRACE_IO(text)
 # define STRACE_DEBUG(text)
 # define STRACE_INFO(text)
 # define STRACE_TEST(text)
 #endif
 
-# define STRACE_WARNING(text) SIMPLE_TRACE(2, "WarninG", text)
+#define STRACE_WARNING(text) SIMPLE_TRACE(2, "WarninG", text)
 #define STRACE_ERROR(text) SIMPLE_TRACE(0,"ERROR", text)
 
 #endif /*_SIMPLE_TRACE_H*/
