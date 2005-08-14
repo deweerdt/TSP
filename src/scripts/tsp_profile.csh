d6668eeb276101dc9d937f4fcf94d3ef7204d8ae
erk <erk>
2005-08-14 23:38:31 +0000

    No more tsp_profile generation, no time, go for TSP_HOME=/opt/tsp, anyone wanting to change would do it by hand

---
+++ src/scripts/tsp_profile.csh
@@ -0,0 +1,3 @@
+#!/bin/csh
+set TSP_HOME /opt/tsp
+setenv PATH $PATH:$TSP_HOME/bin:$TSP_HOME/scripts
---
+++ src/scripts/tsp_profile.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+export TSP_HOME=/opt/tsp
+export PATH=$PATH:$TSP_HOME/bin:$TSP_HOME/scripts
