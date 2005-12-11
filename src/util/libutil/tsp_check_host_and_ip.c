12a7352a4f4988ed9b1e61a06805c1c40e35e392
erk <erk>
2005-12-11 18:58:08 +0000

    Added missing LGPL header

--- src/util/libutil/tsp_check_host_and_ip.c
+++ src/util/libutil/tsp_check_host_and_ip.c
@@ -1,3 +1,39 @@
+/*!  \file 
+
+$Id: tsp_check_host_and_ip.c,v 1.3 2005-12-11 18:58:08 erk Exp $
+
+-----------------------------------------------------------------------
+
+TSP Utils Library - utilities for a generic Transport Sampling Protocol.
+
+Copyright (c) 2002 Yves DUFRENNE, Stephane GALLES, Eric NOULARD and Robert PAGNOT 
+
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 2.1 of the License, or (at your option) any later version.
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with this library; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+-----------------------------------------------------------------------
+
+Project    : TSP
+Maintainer : tsp@astrium.eads.net
+Component  : Utils library
+
+-----------------------------------------------------------------------
+
+Purpose   : check host and ip resolution
+
+-----------------------------------------------------------------------
+ */
 #include <stdio.h>
 #include <netdb.h>
 #include <unistd.h>
