fe223e2e52c67acd37b45a938ca65b1bfde65085
erk <erk>
2007-02-17 16:53:39 +0000

    Improve RPMTools in order to handle to be configured spec files

--- make/UseRPMTools.cmake
+++ make/UseRPMTools.cmake
@@ -1,7 +1,10 @@
 #
 # - Find tools needed for building RPM Packages
 #   on Linux systems and defines macro that helps to
-#   build source or binary RPM
+#   build source or binary RPM, the MACRO assumes
+#   CMake 2.4.x which includes CPack support.
+#   CPack is used to build tar.gz source tarball
+#   which may be used by a custom user-made spec file.
 #
 # - Define RPMTools_ADD_RPM_TARGETS which defines
 #   two (top-level) CUSTOM targets for building
@@ -45,13 +48,6 @@ IF (UNIX)
     #
     MACRO(RPMTools_ADD_RPM_TARGETS RPMNAME SPECFILE_PATH)
 
-      #
-      # We may check here wether if we should
-      # CONFIGURE_FILE or not (FIXME)
-      # 
-      # Check provided SPEC file
-      GET_FILENAME_COMPONENT(SPECFILE_NAME ${SPECFILE_PATH} NAME)
-      
       # Verify whether if RPM_ROOTDIR was provided or not
       IF("${ARGV2}" STREQUAL "") 
 	SET(RPM_ROOTDIR ${CMAKE_BINARY_DIR}/RPM)
@@ -59,7 +55,8 @@ IF (UNIX)
 	SET(RPM_ROOTDIR "${ARGV2}")	
       ENDIF("${ARGV2}" STREQUAL "")
       MESSAGE(STATUS "RPMTools:: Using RPM_ROOTDIR=${RPM_ROOTDIR}")
-      
+
+      # Prepare RPM build tree
       FILE(MAKE_DIRECTORY ${RPM_ROOTDIR})
       FILE(MAKE_DIRECTORY ${RPM_ROOTDIR}/tmp)
       FILE(MAKE_DIRECTORY ${RPM_ROOTDIR}/BUILD)
@@ -67,18 +64,35 @@ IF (UNIX)
       FILE(MAKE_DIRECTORY ${RPM_ROOTDIR}/SOURCES)
       FILE(MAKE_DIRECTORY ${RPM_ROOTDIR}/SPECS)
       FILE(MAKE_DIRECTORY ${RPM_ROOTDIR}/SRPMS)
+
+      #
+      # We check whether if the provided spec file is
+      # to be configure or not.
+      # 
+      GET_FILENAME_COMPONENT(SPECFILE_EXT ${SPECFILE_PATH} EXT)
+      IF (SPECFILE_EXT STREQUAL "spec")
+	# This is a 'ready-to-use' spec file which does not need to be CONFIGURED
+	GET_FILENAME_COMPONENT(SPECFILE_NAME ${SPECFILE_PATH} NAME)
+      ELSE (SPECFILE_EXT STREQUAL "spec")
+	# This is a to-be-configured spec file
+	GET_FILENAME_COMPONENT(SPECFILE_NAME ${SPECFILE_PATH} NAME_WE)
+	SET(SPECFILE_NAME "${SPECFILE_NAME}.spec")
+	MESSAGE(STATUS "Configuring spec file <${SPECFILE_PATH}> --> <${RPM_ROOTDIR}/SPECS/${SPECFILE_NAME}>")
+	CONFIGURE_FILE(
+	  ${SPECFILE_PATH} 
+	  ${RPM_ROOTDIR}/SPECS/${SPECFILE_NAME}
+	  @ONLY)
+      ENDIF (SPECFILE_EXT STREQUAL "spec")
       
       ADD_CUSTOM_TARGET(${RPMNAME}_srpm
 	COMMAND ${CMAKE_MAKE_PROGRAM} package_source
 	COMMAND ${CMAKE_COMMAND} -E copy ${CPACK_SOURCE_PACKAGE_FILE_NAME}.tar.gz ${RPM_ROOTDIR}/SOURCES    
-	COMMAND ${CMAKE_COMMAND} -E copy ${SPECFILE_PATH} ${RPM_ROOTDIR}/SPECS
 	COMMAND ${RPMTools_RPMBUILD_EXECUTABLE} -bs --define=\"_topdir ${RPM_ROOTDIR}\" --buildroot=${RPM_ROOTDIR}/tmp ${RPM_ROOTDIR}/SPECS/${SPECFILE_NAME} 
 	)
       
       ADD_CUSTOM_TARGET(${RPMNAME}_rpm
 	COMMAND ${CMAKE_MAKE_PROGRAM} package_source
 	COMMAND ${CMAKE_COMMAND} -E copy ${CPACK_SOURCE_PACKAGE_FILE_NAME}.tar.gz ${RPM_ROOTDIR}/SOURCES    
-	COMMAND ${CMAKE_COMMAND} -E copy ${SPECFILE_PATH} ${RPM_ROOTDIR}/SPECS
 	COMMAND ${RPMTools_RPMBUILD_EXECUTABLE} -bb --define=\"_topdir ${RPM_ROOTDIR}\" --buildroot=${RPM_ROOTDIR}/tmp ${RPM_ROOTDIR}/SPECS/${SPECFILE_NAME} 
 	)  
     ENDMACRO(RPMTools_ADD_RPM_TARGETS)
