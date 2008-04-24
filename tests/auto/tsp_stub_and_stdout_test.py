38ceca74d9bccf15bb2a21825346991889d027ae
erk <erk>
2008-04-24 16:10:27 +0000

    Small update

--- tests/auto/tsp_stub_and_stdout_test.py
+++ tests/auto/tsp_stub_and_stdout_test.py
@@ -44,7 +44,7 @@ for o, a in opts:
         stdout_param = getUserHostPath(a)
 
 stdout = dtest.DTester("tsp_stdout",
-                       session=dtest.SSHSessionHandler(stdout_param['user'],host=stdout_param['host']))
+                       session=dtest.SSHSessionHandler(stdout_param['user'],host=stdout_param['host'])) 
 stub = dtest.DTester("tsp_stubbed_server",
                      session=dtest.SSHSessionHandler(stdout_param['user'],host=stdout_param['host']))
 # you may change the default time out value
@@ -52,7 +52,7 @@ stub.timeout = 8
 # you add want to save the output of your dtester to a file.
 stub.stdout = file(stub.name + ".out",'w+')
 stub.stdin  = file(stub.name + ".in",'w+')
-#stdout.stdout = file(stdout.name + ".out",'w+')
+stdout.stdout = file(stdout.name + ".out",'w+')
 #stdout.stdin  = file(stdout.name + ".in",'w+')
 
 dtest.DTester.logger.setLevel(level=logging.WARNING)
@@ -64,14 +64,14 @@ stub.addRunStep("barrier","provider started")
 stdout.addRunStep("barrier","provider started")
 stdout.addRunStep("runCommand",command=stdout_param['path']+" -u rpc://"+stub.session.host)
 stdout.addRunStep("expectFromCommand",pattern="tsp_stdout_client: Using provider URL")
-stdout.addRunStep("barrier","consumer started")
 stdout.addRunStep("expectFromCommand",pattern="End of Test OK")
+stdout.addRunStep("ok",stdout.getFutureLastStepStatus(),"First stdout run (no args)")
 stdout.addRunStep("terminateCommand")
-
+stdout.addRunStep("waitCommandTermination")
 stdout.addRunStep("runCommand",command=stdout_param['path']+" -n 150 -u rpc://"+stub.session.host)
 stdout.addRunStep("expectFromCommand",pattern="tsp_stdout_client: Using provider URL")
-stdout.addRunStep("barrier","consumer started 2")
 stdout.addRunStep("expectFromCommand",pattern="End of Test OK")
+stdout.addRunStep("ok",stdout.getFutureLastStepStatus(),"Second stdout run (-n 150) ")
 stdout.addRunStep("terminateCommand")
 stdout.addRunStep("waitCommandTermination")
 stdout.addRunStep("barrier","consumer ended",timeout=1)
