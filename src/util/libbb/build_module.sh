e29a07abd3102306e9f046336bfd80a2b3853033
jaggy <jaggy>
2008-06-26 10:30:44 +0000

    Fix "Out of source build" of linux kernel BB module. (bug #23712) - Improve ADD_CUSTOM_COMMAND DEPENDS options - create a kbuid folder and copy in it every needed files... and then compile locally.

--- src/util/libbb/build_module.sh
+++ src/util/libbb/build_module.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
 # This helper script switches Makefiles between the one needed for the 
 # module and the "main" one
-cp Makefile Makefile.us && cp Makefile.k Makefile && make && mv Makefile.us Makefile
+
+SRC_DIR=$1
+DST_DIR=$2
+
+mkdir -p $DST_DIR/kbuild
+cp $SRC_DIR/Makefile.k $DST_DIR/kbuild/Makefile
+cp $SRC_DIR/bb_alias.h $SRC_DIR/bb_utils.h $SRC_DIR/bb_core.h $SRC_DIR/bb_core_k.h $SRC_DIR/bb_simple.h $DST_DIR/kbuild/
+cp $SRC_DIR/bb_core.c $SRC_DIR/bb_alias.c $SRC_DIR/bb_core_k.c $SRC_DIR/bb_module.c $SRC_DIR/bb_simple.c $DST_DIR/kbuild/
+make -C $DST_DIR/kbuild
